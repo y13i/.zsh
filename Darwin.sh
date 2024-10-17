f4599b7cd0dd2d6d1787635664711f11e05b233c
Yoriki Yamaguchi <7214104+y13i@users.noreply.github.com>
2024-10-17 16:52:26 +0900

    Update Darwin.sh

--- Darwin.sh
+++ Darwin.sh
@@ -1,6 +1,6 @@
 bindkey '^P' history-substring-search-up
 bindkey '^N' history-substring-search-down
 
-export PNPM_HOME="/Users/y13i/Library/pnpm"
+export PNPM_HOME="$HOME/Library/pnpm"
 
 [[ -f "/opt/homebrew/bin/brew" ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
